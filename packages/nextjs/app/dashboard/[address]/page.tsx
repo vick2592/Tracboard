0d982b1cff8c839485bc2807c34ce44640d0e092
Victor <vick2592@yahoo.com>
2024-04-21 17:06:19 -0400

    Fifth Commit

--- packages/nextjs/app/dashboard/[address]/page.tsx
+++ packages/nextjs/app/dashboard/[address]/page.tsx
@@ -1,15 +1,158 @@
-'use client'
+"use client";
+
+import { useEffect, useState } from "react";
+import Moralis from "moralis";
+import { useAccount } from "wagmi";
+
+// import { AdjustmentsHorizontalIcon, BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
+// Moralis.start({ apiKey: process.env.NEXT_PUBLIC_MORALIS_API_KEY }); // Initialize Moralis here
 
 const Dashboard = ({ params }) => {
-    const address = params?.address;
+  const addressHook = params?.address;
+  const { address: connectedAddress, chain, chainId } = useAccount();
+  const chainID = chainId;
+  const chainName = chain?.name;
+  const [balances, setBalances] = useState(null);
+  const [currentTab, setCurrentTab] = useState("tokens"); // Add this line
+  const [balancesNFT, setBalancesNFT] = useState(null); // Add this line
+
+  useEffect(() => {
+    if (!window.MoralisStarted) {
+      Moralis.start({ apiKey: process.env.NEXT_PUBLIC_MORALIS_API_KEY }); // Initialize Moralis here
+      window.MoralisStarted = true;
+    }
+  }, []);
+
+  useEffect(() => {
+    const fetchBalances = async () => {
+      if (!connectedAddress) return; // Don't fetch balances if connectedAddress is not available
+      const ChainId = `0x${chainID}`; // Move this line here
+      try {
+        // const response = await Moralis.EvmApi.token.getWalletTokenBalances({
+        const response = await Moralis.EvmApi.wallets.getWalletTokenBalancesPrice({
+          chain: ChainId,
+          address: addressHook,
+        });
+        const responseNFT = await Moralis.EvmApi.nft.getWalletNFTs({
+          chain: ChainId,
+          format: "decimal",
+          mediaItems: true,
+          address: addressHook,
+        });
+
+        setBalances(response.result);
+        setBalancesNFT(responseNFT.result);
+        // console.log("The response is:", response.result);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    fetchBalances();
+  }, [chainID, connectedAddress, addressHook]); // Add connectedAddress to the dependency array
+
+  console.log(balances);
+  console.log(balancesNFT);
+  console.log(addressHook);
 
   return (
     <>
-      <div className="text-center mt-8 bg-secondary p-10">
-        <h1 className="text-4xl my-0">Moralis Powered Dashboard</h1>
+      <div className="text-center bg-secondary p-10">
+        <h1 className="text-4xl text-center my-0">Moralis Powered Dashboard</h1>
+        <p className="text-neutral">Wallet Displayed: {addressHook}</p>
         <p className="text-neutral">
-        {address} 
+          Chain ID: {chainID} {chainName}
         </p>
+
+        {/* Add these lines */}
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2"
+          onClick={() => setCurrentTab("tokens")}
+        >
+          View Tokens
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4"
+          onClick={() => setCurrentTab("nfts")}
+        >
+          View NFTs
+        </button>
+
+        {balances && currentTab === "tokens" && (
+          <div className="flex justify-center px-4 md:px-0">
+            <div className="overflow-x-auto w-full shadow-2xl">
+              <table className="table text-xl bg-base-100 w-full md:table-md table-sm">
+                <thead>
+                  <tr className="rounded-xl text-sm text-base-content">
+                    <th className="bg-primary p-2 border border-gray-300">Token</th>
+                    <th className="bg-primary p-2 border border-gray-300">Name</th>
+                    <th className="bg-primary p-2 border border-gray-300">Price</th>
+                    <th className="bg-primary p-2 border border-gray-300">% Change</th>
+                    <th className="bg-primary p-2 border border-gray-300">Balance</th>
+                    <th className="bg-primary p-2 border border-gray-300">% Portfolio</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Array.isArray(balances) &&
+                    balances.map((balance, index) => (
+                      <tr key={index} className="hover:bg-green-200 hover:text-black text-sm">
+                        <td className="w-1/3 p-2 border border-gray-300">
+                          <div className="flex items-center">
+                            <img src={balance.logo} width="15%" className="mr-2" />
+                            <span>{balance.symbol}</span>
+                          </div>
+                        </td>
+                        <td className="w-1/3 p-2 border border-gray-300">{balance.name}</td>
+                        <td className="w-1/3 p-2 border border-gray-300">{parseFloat(balance.usdPrice).toFixed(2)}</td>
+                        <td className="w-1/3 p-2 border border-gray-300">
+                          {parseFloat(balance.usdPrice24hrPercentChange).toFixed(2)}
+                        </td>
+                        <td className="w-1/3 p-2 border border-gray-300">
+                          {parseFloat(balance.balanceFormatted).toFixed(2)}
+                        </td>
+                        <td className="w-1/3 p-2 border border-gray-300">
+                          {parseFloat(balance.portfolioPercentage).toFixed(2)}
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        )}
+        {Array.isArray(balancesNFT) && currentTab === "nfts" && (
+          <div className="flex justify-center px-4 md:px-0">
+            <div className="overflow-x-auto w-full shadow-2xl">
+              {balancesNFT.map((balance, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="bg-base-100 w-full m-1 md:w-md hover:bg-green-200 hover:text-black text-sm p-4 mb-4"
+                  >
+                    <div className="text-xl text-base-content bg-primary p-2 border border-gray-300">NFT</div>
+                    <div className="flex items-center justify-center">
+                      <img src={balance._data.metadata.image} style={{ maxWidth: "100%" }} className="my-2" />
+                    </div>
+                    <div className="text-xl text-base-content bg-primary p-2 border border-gray-300">Name</div>
+                    <div className="w-full p-2 border border-gray-300">{balance._data.metadata.name}</div>
+                    <div className="text-xl text-base-content bg-primary p-2 border border-gray-300">Description</div>
+                    <div className="w-full p-2 border border-gray-300">{balance._data.metadata.description}</div>
+                    <div className="text-xl text-base-content bg-primary p-2 border border-gray-300">Contract Type</div>
+                    <div className="w-full p-2 border border-gray-300">{balance._data.contractType}</div>
+                    <div className="text-xl text-base-content bg-primary p-2 border border-gray-300">Token ID</div>
+                    <div className="w-full p-2 border border-gray-300 overflow-auto">{balance._data.tokenId}</div>
+                    <div className="text-xl text-base-content bg-primary p-2 border border-gray-300 ">
+                      Token Address
+                    </div>
+                    <div className="w-full p-2 border border-gray-300 overflow-auto">
+                      {balance._data.tokenAddress._value}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,45 +1,19 @@
 "use client";
 
-import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon, AdjustmentsHorizontalIcon } from "@heroicons/react/24/outline";
+import { AdjustmentsHorizontalIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
-import { useState, useEffect } from 'react';
-import Moralis from 'moralis';
-import * as chains from "viem/chains";
-import { useRouter } from 'next/navigation'
 import { InputBase } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
   const router = useRouter();
   const { address: connectedAddress } = useAccount();
-  const [balances, setBalances] = useState(null);
   const [addressLook, setAddressLook] = useState<string>();
 
-  useEffect(() => {
-    const fetchBalances = async () => {
-      if (!connectedAddress) return; // Don't fetch balances if connectedAddress is not available
-
-      const chainId = `0x${chains.mainnet.id}`; // Move this line here
-
-      try {
-        await Moralis.start({ apiKey: process.env.NEXT_PUBLIC_MORALIS_API_KEY });
-        const response = await Moralis.EvmApi.token.getWalletTokenBalances({
-          "chain": chainId,
-          "address": connectedAddress
-        });
-        setBalances(response.raw);
-      } catch (e) {
-        console.error(e);
-      }
-    };
-    fetchBalances();
-  }, [connectedAddress]); // Add connectedAddress to the dependency array
-
-  console.log(balances);
-  console.log(connectedAddress)
-  console.log(chains.mainnet.id)
+  // console.log(connectedAddress)
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
@@ -52,30 +26,29 @@ const Home: NextPage = () => {
             <p className="my-2 font-medium">Connected Address:</p>
             <Address address={connectedAddress} />
           </div>
-
         </div>
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-             <AdjustmentsHorizontalIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your wallet your way.
-              </p>
+              <AdjustmentsHorizontalIcon className="h-8 w-8 fill-secondary" />
+              <p>Explore your wallet your way.</p>
               <div className="flex justify-center items-center gap-2">
-                <button onClick={() => router.push(`/dashboard/${connectedAddress}`)} className="btn btn-primary">View dashboard</button>
+                <button onClick={() => router.push(`/dashboard/${connectedAddress}`)} className="btn btn-primary">
+                  View dashboard
+                </button>
               </div>
             </div>
 
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-            <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Enter wallet address to view.
-              </p>
-      
+              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
+              <p>Enter wallet address to view.</p>
+
               <InputBase name="address" placeholder="address" value={addressLook} onChange={setAddressLook} />
               <div className="flex justify-center items-center gap-2 p-4">
-                <button onClick={() => router.push(`/dashboard/${addressLook}`)} className="btn btn-primary">Search</button>
+                <button onClick={() => router.push(`/dashboard/${addressLook}`)} className="btn btn-primary">
+                  Search
+                </button>
               </div>
             </div>
           </div>
